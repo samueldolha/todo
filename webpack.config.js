f8a5be4c57fc7385255829add8dc0f5bea5992d7
Samuel Dolha <samueldolha@gmail.com>
2019-05-08 00:23:40 -0600

    Support CSS modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   module: {
@@ -8,12 +9,26 @@ module.exports = {
       {
         test: /\.js$/u,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/u,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[folder]-[local]'
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin()
   ]
 };
