88fd16d4d4fc27db03f89b7c2c784c040758415d
Samuel Dolha <samueldolha@gmail.com>
2019-05-09 01:07:57 -0600

    Test clearing input after adding todo

---
+++ src/components/TodoEntry/spec.js
@@ -0,0 +1,17 @@
+import { h } from 'preact';
+import { cleanup, fireEvent, render } from 'preact-testing-library';
+import TodoEntry from '.';
+
+it('clears its input after adding a todo', async () => {
+  const {
+    getByLabelText,
+    getByText,
+    queryByDisplayValue
+  } = render(<TodoEntry onAddTodo={() => null} />);
+  const value = 'buy groceries';
+  await fireEvent.change(getByLabelText(/todo/iu), { target: { value } });
+  expect(queryByDisplayValue(value)).not.toBeNull();
+  await fireEvent.click(getByText(/add/iu));
+  expect(queryByDisplayValue(value)).toBeNull();
+  cleanup();
+});
