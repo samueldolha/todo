4700d0306e3903e0d68add5f0922a1fba9b18cbb
Samuel Dolha <samueldolha@gmail.com>
2019-05-09 14:56:55 -0600

    Extract setUp function

--- src/components/TodoEntry/spec.js
+++ src/components/TodoEntry/spec.js
@@ -2,14 +2,19 @@ import { h } from 'preact';
 import { cleanup, fireEvent, render } from 'preact-testing-library';
 import TodoEntry from '.';
 
+const setUp = () => {
+  const spy = jest.fn();
+
+  return {
+    render: render(<TodoEntry onAddTodo={spy} />),
+    spy
+  };
+};
+
 afterEach(cleanup);
 
 it('clears its input after adding a todo', async () => {
-  const spy = jest.fn();
-  const {
-    getByLabelText,
-    queryAllByDisplayValue
-  } = render(<TodoEntry onAddTodo={spy} />);
+  const { render: { getByLabelText, queryAllByDisplayValue }, spy } = setUp();
   const inputField = getByLabelText(/todo/iu);
   const value = 'buy groceries';
   await fireEvent.input(inputField, { target: { value } });
@@ -20,8 +25,7 @@ it('clears its input after adding a todo', async () => {
 });
 
 it('prevents adding an empty todo', () => {
-  const spy = jest.fn();
-  const { queryAllByDisplayValue } = render(<TodoEntry onAddTodo={spy} />);
+  const { render: { queryAllByDisplayValue }, spy } = setUp();
   expect(queryAllByDisplayValue('').length).toBe(1);
   expect(spy).toBeCalledTimes(0);
 });
