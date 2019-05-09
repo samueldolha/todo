8961a20c0d3a55ef8fb88b232aa127bfdb6f6f8c
Samuel Dolha <samueldolha@gmail.com>
2019-05-09 00:10:10 -0600

    Await simulated DOM events

--- src/components/App/spec.js
+++ src/components/App/spec.js
@@ -1,13 +1,14 @@
+import '@babel/polyfill';
 import { h } from 'preact';
 import { cleanup, fireEvent, render } from 'preact-testing-library';
 import App from '.';
 
-it('adds a todo to the list', () => {
+it('adds a todo to the list', async () => {
   const { getByLabelText, getByText, queryByText } = render(<App />);
   const value = 'buy groceries';
   expect(queryByText(value)).toBeNull();
-  fireEvent.change(getByLabelText(/todo/iu), { target: { value } });
-  fireEvent.click(getByText(/add/iu));
+  await fireEvent.change(getByLabelText(/todo/iu), { target: { value } });
+  await fireEvent.click(getByText(/add/iu));
   expect(queryByText(value)).not.toBeNull();
   cleanup();
 });
