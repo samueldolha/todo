3ac97a8d3fbbe3b827c9c5642f4d68b2deeb50ad
Samuel Dolha <samueldolha@gmail.com>
2019-05-09 15:26:53 -0600

    Improve variable name

--- src/components/TodoEntry/spec.js
+++ src/components/TodoEntry/spec.js
@@ -3,29 +3,32 @@ import { cleanup, fireEvent, render } from 'preact-testing-library';
 import TodoEntry from '.';
 
 const setUp = () => {
-  const spy = jest.fn();
+  const onAddTodo = jest.fn();
 
   return {
-    render: render(<TodoEntry onAddTodo={spy} />),
-    spy
+    onAddTodo,
+    render: render(<TodoEntry onAddTodo={onAddTodo} />)
   };
 };
 
 afterEach(cleanup);
 
 it('clears its input after adding a todo', async () => {
-  const { render: { getByLabelText, queryAllByDisplayValue }, spy } = setUp();
+  const {
+    onAddTodo,
+    render: { getByLabelText, queryAllByDisplayValue }
+  } = setUp();
   const inputField = getByLabelText(/todo/iu);
   const value = 'buy groceries';
   await fireEvent.input(inputField, { target: { value } });
   expect(queryAllByDisplayValue(value).length).toBe(1);
   await fireEvent.keyDown(inputField, { key: 'Enter' });
-  expect(spy).toBeCalledTimes(1);
+  expect(onAddTodo).toBeCalledTimes(1);
   expect(queryAllByDisplayValue(value).length).toBe(0);
 });
 
 it('prevents adding an empty todo', () => {
-  const { render: { queryAllByDisplayValue }, spy } = setUp();
+  const { onAddTodo, render: { queryAllByDisplayValue } } = setUp();
   expect(queryAllByDisplayValue('').length).toBe(1);
-  expect(spy).toBeCalledTimes(0);
+  expect(onAddTodo).toBeCalledTimes(0);
 });
