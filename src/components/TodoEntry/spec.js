7867ccad688db7d6c79929190da8caec78b677f5
Samuel Dolha <samueldolha@gmail.com>
2019-05-09 19:11:20 -0600

    Test TodoEntry prop types

--- src/components/TodoEntry/spec.js
+++ src/components/TodoEntry/spec.js
@@ -1,36 +1,56 @@
 import { h } from 'preact';
 import { cleanup, fireEvent, render } from 'preact-testing-library';
+import testPropTypes from '../../utility/testPropTypes';
 import TodoEntry from '.';
 
-const setUp = () => {
-  const onAddTodo = jest.fn();
-
-  return {
-    onAddTodo,
-    render: render(<TodoEntry onAddTodo={onAddTodo} />)
-  };
-};
-
-afterEach(() => {
-  cleanup();
-});
-
-it('clears its input after adding a todo', async () => {
+describe('props', () => {
   const {
-    onAddTodo,
-    render: { getByLabelText, queryAllByDisplayValue }
-  } = setUp();
-  const inputField = getByLabelText(/todo/iu);
-  const value = 'buy groceries';
-  await fireEvent.input(inputField, { target: { value } });
-  expect(queryAllByDisplayValue(value).length).toBe(1);
-  await fireEvent.keyDown(inputField, { key: 'Enter' });
-  expect(onAddTodo).toBeCalledTimes(1);
-  expect(queryAllByDisplayValue(value).length).toBe(0);
+    expectRequired,
+    expectType
+  } = testPropTypes(TodoEntry, { onAddTodo: () => null });
+
+  describe('onAddTodo', () => {
+    it('is required', () => {
+      expectRequired('onAddTodo');
+    });
+
+    it('is a function', () => {
+      expectType('onAddTodo');
+    });
+  });
 });
 
-it('prevents adding an empty todo', () => {
-  const { onAddTodo, render: { queryAllByDisplayValue } } = setUp();
-  expect(queryAllByDisplayValue('').length).toBe(1);
-  expect(onAddTodo).toBeCalledTimes(0);
+describe('behavior', () => {
+  const setUp = () => {
+    const onAddTodo = jest.fn();
+
+    return {
+      onAddTodo,
+      render: render(<TodoEntry onAddTodo={onAddTodo} />)
+    };
+  };
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('clears its input after adding a todo', async () => {
+    const {
+      onAddTodo,
+      render: { getByLabelText, queryAllByDisplayValue }
+    } = setUp();
+    const inputField = getByLabelText(/todo/iu);
+    const value = 'buy groceries';
+    await fireEvent.input(inputField, { target: { value } });
+    expect(queryAllByDisplayValue(value).length).toBe(1);
+    await fireEvent.keyDown(inputField, { key: 'Enter' });
+    expect(onAddTodo).toBeCalledTimes(1);
+    expect(queryAllByDisplayValue(value).length).toBe(0);
+  });
+
+  it('prevents adding an empty todo', () => {
+    const { onAddTodo, render: { queryAllByDisplayValue } } = setUp();
+    expect(queryAllByDisplayValue('').length).toBe(1);
+    expect(onAddTodo).toBeCalledTimes(0);
+  });
 });
