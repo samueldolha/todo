b546a0479226b2070c368f946bb596dc964aed2b
Samuel Dolha <samueldolha@gmail.com>
2019-05-09 19:45:19 -0600

    Test TodoList prop types

--- src/components/TodoList/spec.js
+++ src/components/TodoList/spec.js
@@ -1,18 +1,38 @@
 import { h } from 'preact';
 import { cleanup, render } from 'preact-testing-library';
 import immutable from 'immutable';
+import testPropTypes from '../../utility/testPropTypes';
 import TodoList from '.';
 
-afterEach(() => {
-  cleanup();
+describe('props', () => {
+  const {
+    expectRequired,
+    expectInstanceOf
+  } = testPropTypes(TodoList, { todos: immutable.List() });
+
+  describe('todos', () => {
+    it('is required', () => {
+      expectRequired('todos');
+    });
+
+    it('is an Immutable.js List', () => {
+      expectInstanceOf('todos');
+    });
+  });
 });
 
-it('displays the todos', () => {
-  const firstTodo = 'buy groceries';
-  const secondTodo = 'walk the dog';
-  const {
-    queryAllByText
-  } = render(<TodoList todos={immutable.List([firstTodo, secondTodo])} />);
-  expect(queryAllByText(firstTodo).length).toBe(1);
-  expect(queryAllByText(secondTodo).length).toBe(1);
+describe('behavior', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('displays the todos', () => {
+    const firstTodo = 'buy groceries';
+    const secondTodo = 'walk the dog';
+    const {
+      queryAllByText
+    } = render(<TodoList todos={immutable.List([firstTodo, secondTodo])} />);
+    expect(queryAllByText(firstTodo).length).toBe(1);
+    expect(queryAllByText(secondTodo).length).toBe(1);
+  });
 });
