a8ebb19f5364bd73d866d82a28fc40cd00292c4c
Samuel Dolha <samueldolha@gmail.com>
2019-05-16 23:38:16 -0600

    Extract createExpecter function

--- src/utility/testPropTypes/index.js
+++ src/utility/testPropTypes/index.js
@@ -2,64 +2,66 @@ import PropTypes from 'prop-types';
 
 const location = 'prop';
 
-const expectRequired = (Component, validProps) => (key) => {
+const createExpecter = (expecter) => (key) => {
   const consoleError = jest.spyOn(console, 'error').mockImplementation();
-  PropTypes.checkPropTypes(
-    // eslint-disable-next-line react/forbid-foreign-prop-types
-    Component.propTypes,
-    {
-      ...validProps,
-      [key]: null
-    },
-    location,
-    Component.displayName
-  );
-  expect(consoleError).toBeCalledWith(`Warning: Failed ${location} type:`
-    + ` The ${location} \`${key}\` is marked as required in`
-    + ` \`${Component.displayName}\`, but its value is \`null\`.`);
+  expecter(consoleError, key);
   PropTypes.resetWarningCache();
   consoleError.mockRestore();
 };
 
-const expectType = (Component, validProps) => (key) => {
-  const consoleError = jest.spyOn(console, 'error').mockImplementation();
-  PropTypes.checkPropTypes(
-    // eslint-disable-next-line react/forbid-foreign-prop-types
-    Component.propTypes,
-    {
-      ...validProps,
-      [key]: Symbol(key)
-    },
-    location,
-    Component.displayName
-  );
-  expect(consoleError).toBeCalledWith(`Warning: Failed ${location} type:`
-    + ` Invalid ${location} \`${key}\` of type \`symbol\` supplied to`
-    + ` \`${Component.displayName}\`, expected \`${typeof validProps[key]}\`.`);
-  PropTypes.resetWarningCache();
-  consoleError.mockRestore();
-};
+const expectRequired
+  = (Component, validProps) => createExpecter((consoleError, key) => {
+    PropTypes.checkPropTypes(
+      // eslint-disable-next-line react/forbid-foreign-prop-types
+      Component.propTypes,
+      {
+        ...validProps,
+        [key]: null
+      },
+      location,
+      Component.displayName
+    );
+    expect(consoleError).toBeCalledWith(`Warning: Failed ${location} type:`
+      + ` The ${location} \`${key}\` is marked as required in`
+      + ` \`${Component.displayName}\`, but its value is \`null\`.`);
+  });
 
-const expectInstanceOf = (Component, validProps) => (key) => {
-  const consoleError = jest.spyOn(console, 'error').mockImplementation();
-  const Placeholder = () => null;
-  PropTypes.checkPropTypes(
-    // eslint-disable-next-line react/forbid-foreign-prop-types
-    Component.propTypes,
-    {
-      ...validProps,
-      [key]: new Placeholder()
-    },
-    location,
-    Component.displayName
-  );
-  expect(consoleError).toBeCalledWith(`Warning: Failed ${location} type:`
-    + ` Invalid ${location} \`${key}\` of type \`${Placeholder.name}\``
-    + ` supplied to \`${Component.displayName}\`,`
-    + ` expected instance of \`${validProps[key].constructor.name}\`.`);
-  PropTypes.resetWarningCache();
-  consoleError.mockRestore();
-};
+const expectType
+  = (Component, validProps) => createExpecter((consoleError, key) => {
+    PropTypes.checkPropTypes(
+      // eslint-disable-next-line react/forbid-foreign-prop-types
+      Component.propTypes,
+      {
+        ...validProps,
+        [key]: Symbol(key)
+      },
+      location,
+      Component.displayName
+    );
+    expect(consoleError).toBeCalledWith(`Warning: Failed ${location} type:`
+      + ` Invalid ${location} \`${key}\` of type \`symbol\` supplied to`
+      + ` \`${Component.displayName}\`,`
+      + ` expected \`${typeof validProps[key]}\`.`);
+  });
+
+const expectInstanceOf
+  = (Component, validProps) => createExpecter((consoleError, key) => {
+    const Placeholder = () => null;
+    PropTypes.checkPropTypes(
+      // eslint-disable-next-line react/forbid-foreign-prop-types
+      Component.propTypes,
+      {
+        ...validProps,
+        [key]: new Placeholder()
+      },
+      location,
+      Component.displayName
+    );
+    expect(consoleError).toBeCalledWith(`Warning: Failed ${location} type:`
+      + ` Invalid ${location} \`${key}\` of type \`${Placeholder.name}\``
+      + ` supplied to \`${Component.displayName}\`,`
+      + ` expected instance of \`${validProps[key].constructor.name}\`.`);
+  });
 
 export default (Component, validProps) => {
   if (typeof Component !== 'function') {
