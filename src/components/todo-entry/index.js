1c6013aedda60e37b318f2c19a8e1e6a48a17bc0
Samuel Dolha <samueldolha@gmail.com>
2019-05-21 10:29:02 -0600

    Remove obsolete Fragment tag

--- src/components/todo-entry/index.js
+++ src/components/todo-entry/index.js
@@ -1,4 +1,3 @@
-import { Fragment } from 'preact';
 import { useCallback, useState } from 'preact/hooks';
 import PropTypes from 'prop-types';
 
@@ -6,32 +5,30 @@ const TodoEntry = ({ onAddTodo }) => {
   const [input, setInput] = useState('');
 
   return (
-    <Fragment>
-      <label>
-        {'Enter a todo:'}
-        {' '}
-        <input
-          autoFocus
-          onInput={useCallback(
-            (event) => {
-              setInput(event.target.value);
-            },
-            []
-          )}
-          onKeyDown={useCallback(
-            (event) => {
-              if (event.key === 'Enter' && input !== '') {
-                onAddTodo(input);
-                setInput('');
-              }
-            },
-            [input, onAddTodo]
-          )}
-          type="text"
-          value={input}
-        />
-      </label>
-    </Fragment>
+    <label>
+      {'Enter a todo:'}
+      {' '}
+      <input
+        autoFocus
+        onInput={useCallback(
+          (event) => {
+            setInput(event.target.value);
+          },
+          []
+        )}
+        onKeyDown={useCallback(
+          (event) => {
+            if (event.key === 'Enter' && input !== '') {
+              onAddTodo(input);
+              setInput('');
+            }
+          },
+          [input, onAddTodo]
+        )}
+        type="text"
+        value={input}
+      />
+    </label>
   );
 };
 
