1671bf3ef0086c5ba114962db90f5bda69a186e0
Samuel Dolha <samueldolha@gmail.com>
2019-05-23 14:12:28 -0600

    Spread arguments in action call

--- src/use-dispatch/index.js
+++ src/use-dispatch/index.js
@@ -2,7 +2,7 @@ import { useCallback } from "preact/hooks";
 
 export default (dispatch, action) => useCallback(
   (...parameters) => {
-    dispatch(action(parameters));
+    dispatch(action(...parameters));
   },
   [dispatch, action]
 );
