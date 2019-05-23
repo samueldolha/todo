beada65556d5b7318ecf4e1da075f442a9410918
Samuel Dolha <samueldolha@gmail.com>
2019-05-23 17:12:26 -0600

    Extract value variable

--- src/test-prop-types/expect-type.js
+++ src/test-prop-types/expect-type.js
@@ -3,19 +3,20 @@ import PropTypes from "prop-types";
 
 export default (context, key) => {
   const location = "prop";
+  const value = Symbol(key);
   PropTypes.checkPropTypes(
     // eslint-disable-next-line react/forbid-foreign-prop-types
     context.Component.propTypes,
     {
       ...context.validProps,
-      [key]: Symbol(key)
+      [key]: value
     },
     location,
     context.Component.displayName
   );
   expect(context.consoleError).toBeCalledWith("Warning: Failed"
     + ` ${location} type:`
-    + ` Invalid ${location} \`${key}\` of type \`symbol\` supplied to`
+    + ` Invalid ${location} \`${key}\` of type \`${typeof value}\` supplied to`
     + ` \`${context.Component.displayName}\`,`
     + ` expected \`${typeof context.validProps[key]}\`.`);
 };
